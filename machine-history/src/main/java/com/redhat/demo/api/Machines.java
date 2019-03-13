9b1c12c24dd4626705cf4057b085eb73e29c2ca7
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-03-13 15:56:17 +1100

    Apicurio update

---
+++ machine-history/src/main/java/com/redhat/demo/api/Machines.java
@@ -0,0 +1,24 @@
+package com.redhat.demo.api;
+
+import com.redhat.demo.api.beans.MachineState;
+import java.lang.Integer;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/machines")
+public interface Machines {
+  @GET
+  @Produces("application/json")
+  List<Integer> allMachines();
+
+  @Path("/{id}")
+  @GET
+  @Produces("application/json")
+  MachineState get(@PathParam("id") Integer id);
+}
