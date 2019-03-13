dbc3ac9618f65da6473ca9c0550cd197ab07507e
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-03-13 12:25:39 +1100

    test

---
+++  /src/main/java/com/redhat/demo/api/Machine.java
@@ -0,0 +1,22 @@
+package com.redhat.demo.api;
+
+import java.lang.Integer;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/machine")
+public interface Machine {
+  @GET
+  @Produces("application/json")
+  List<Integer> getsAListOfAllTheMachines();
+
+  @Path("/{id}")
+  @GET
+  void generatedMethod1(@PathParam("id") Integer id);
+}
