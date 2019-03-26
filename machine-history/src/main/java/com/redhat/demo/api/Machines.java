b9a45a07999d39b6c468fc1298550c2fe682d2db
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-03-27 00:14:15 +1100

    Import API

--- machine-history/src/main/java/com/redhat/demo/api/Machines.java
+++ machine-history/src/main/java/com/redhat/demo/api/Machines.java
@@ -1,6 +1,7 @@
 package com.redhat.demo.api;
 
 import com.redhat.demo.api.beans.Machine;
+import com.redhat.demo.api.beans.MachineState;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -14,4 +15,9 @@ public interface Machines {
   @GET
   @Produces("application/json")
   List<Machine> getAllMachines();
+
+  @Path("/{id}")
+  @GET
+  @Produces("application/json")
+  MachineState getMachine();
 }
