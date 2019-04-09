04352e60feef57d6098066526c52c12bfe33f642
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-04-09 00:55:18 -0400

    Import API

---
+++ machine-history/src/main/java/com/redhat/demo/api/MachinesResource.java
@@ -0,0 +1,25 @@
+package com.redhat.demo.api;
+
+import com.redhat.demo.api.beans.Machine;
+import com.redhat.demo.api.beans.MachineHistory;
+import java.lang.Integer;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/machines")
+public interface MachinesResource {
+  @GET
+  @Produces("application/json")
+  List<Machine> getAllMachines();
+
+  @Path("/history/{id}")
+  @GET
+  @Produces("application/json")
+  MachineHistory machineHistory(@PathParam("id") Integer id);
+}
